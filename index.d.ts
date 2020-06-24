39505c35b436b1887b1a8d1072a07935aee1c1e7
Kirill Shekhovtsov <ksedline@gmail.com>
2020-06-25 01:42:02 +0300

    Add definitions in typings

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,17 @@
+import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+
 declare module 'react-native-view-slider' {
   export default class ViewSlider extends React.Component<{
+    height?: string, 
     style?: StyleProp<ViewStyle>,
     dotsContainerStyle?: StyleProp<ViewStyle>,
     dotActiveColor?: string,
     dotInactiveColor?: string,
     dots?: boolean,
-    renderSlides: JSX.Element, 
-    height?: string, 
     autoSlide?: boolean, 
     slideInterval?: number, 
     slideCount?: number
+    renderSlides: JSX.Element, 
   }> {}
 }
