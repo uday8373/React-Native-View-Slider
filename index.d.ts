19a8a4a88d29da1435d11148b4f848cb9d56463e
Kirill Shekhovtsov <ksedline@gmail.com>
2020-06-25 01:37:12 +0300

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,14 @@
-interface IProps {
-  style?: StyleProp<ViewStyle>;
-  dotsContainerStyle?: StyleProp<ViewStyle>;
-  dotActiveColor?: string;
-  dotInactiveColor?: string;
-  slideCount?: number;
-  dots?: boolean;
-  renderSlides: JSX.Element;
-  height?: string;
-  autoSlide?: boolean;
-  slideInterval?: number;
-  slideCount?: number;
-}
-
 declare module 'react-native-view-slider' {
-  export default class ViewSlider extends React.Component<IProps> {}
+  export default class ViewSlider extends React.Component<{
+    style?: StyleProp<ViewStyle>,
+    dotsContainerStyle?: StyleProp<ViewStyle>,
+    dotActiveColor?: string,
+    dotInactiveColor?: string,
+    dots?: boolean,
+    renderSlides: JSX.Element, 
+    height?: string, 
+    autoSlide?: boolean, 
+    slideInterval?: number, 
+    slideCount?: number
+  }> {}
 }
